351f62047477a438ef8a265da995ab0bcffc3dda
Steve Bennett <steveben@fb.com>
2021-08-05 17:41:59 +1000

    Refactor.

--- README.md
+++ README.md
@@ -1,30 +1,12 @@
 # Community map template
 
-This is a simple template for a pin-based map that draws its content from a public Google Sheets worksheet.
+This is a simple template for Maplibre/Mapbox based map using VueJS.
 
 ## Project setup
 
 1. `yarn install`
 2. Change the project name in package.json
-3. Get an account on Mapbox, and replace the public token in Map.vue with your public token
-
-## Using a Google Sheet for data
-
-This project starts using data in https://docs.google.com/spreadsheets/d/1DFXQYOh2CRZbFtMAFNbVJwpWQxx_SUhPr56OYaDLWS0/edit?usp=sharing
-
-1. Duplicate this Google Sheet and use it as a template. 
-2. Under File, "Publish to the web", as CSV.
-3. Put the CSV link in Map.vue.
-
-### Special columns
-
-The template recognises certain column names by default. Obviously you can change these in the code.
-
-* `Longitude`, `Latitude`
-* `Name`: title of feature info panel
-* `Description`: smaller text in feature info panel
-* `Image_Url`: 
-
+3. Get an account on Mapbox, and replace the public token in Map.vue with your public token. Or use a non-Mapbox source for your tiles.
 
 ## Running locally
 ```
@@ -39,7 +21,7 @@ npm run serve
 
 ### Customising
 
-There aren't many components. 
+There aren't many components.
 
 * App.vue: Overall structure of the site
 * FeatureInfo.vue: Information shown when you click on something
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,53 +3,61 @@
 </template>
 
 <script>
-import mapboxgl from "mapbox-gl";
-import U from "mapbox-gl-utils";
-import { sheets2geojson } from "sheets2geojson";
-import { EventBus } from "../EventBus";
+import mapboxgl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import U from 'mapbox-gl-utils';
+import { EventBus } from '../EventBus';
 export default {
     async mounted() {
         // replace this Mapbox access token with your own
         mapboxgl.accessToken =
-            "pk.eyJ1Ijoic3RldmFnZSIsImEiOiJjazNmNGV5enAwMTF1M2tuejhtc2twcXo5In0.mLPrYIYJ2FiFZ3KMqVIj6w";
+            'pk.eyJ1Ijoic3RldmFnZSIsImEiOiJjazNmNGV5enAwMTF1M2tuejhtc2twcXo5In0.mLPrYIYJ2FiFZ3KMqVIj6w';
         const map = new mapboxgl.Map({
-            container: "map",
+            container: 'map',
             center: [144.96, -37.81],
             zoom: 14,
-            style: "mapbox://styles/mapbox/light-v9"
+            style: 'mapbox://styles/mapbox/light-v9',
         });
         U.init(map, mapboxgl);
+
         window.map = map;
+        this.map = map;
         window.app.Map = this;
+        await map.U.onLoad();
+        this.initMapContent(map);
+    },
+    methods: {
+        initMapContent(map) {
+            const points = {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [144.96, -37.81],
+                        },
+                        properties: {},
+                    },
+                ],
+            };
 
-        // const sheetNo = 1;
-        // I don't know in which context json1 works vs json2.
-        // https://medium.com/dali-lab/google-sheets-and-json-easy-backend-e29e9ef3df2
-        // const json1 = `https://spreadsheets.google.com/feeds/cells/${sheetsId}/${sheetNo}/public/full?alt=json`
-        // const json2 = `https://spreadsheets.google.com/feeds/cells/${sheetsId}/${sheetNo}/od6/public/values?alt=json`
-        const sheetID =
-            (window.location.hash.match(/sheet=([a-zA-Z0-9_-]+)/) || [])[1] ||
-            "2PACX-1vR9qYMju-qqH_IL8e2ksN0wpVXfHBUEKF079WX1eSAgPFRG5z0RAmpjVwS8sVrZSC0fVrNpSMjaB5Cu";
-        const points = await sheets2geojson(sheetID);
-
-        map.U.addGeoJSON("points", points);
-        map.U.addCircle("points-circles", "points", {
-            circleColor: "hsl(330,100%,40%)",
-            circleRadius: {
-                stops: [
-                    [10, 3],
-                    [12, 10]
-                ]
-            }
-        });
-        map.U.hoverPointer("points-circles");
-        map.on("click", "points-circles", e => {
-            console.log(e);
-            EventBus.$emit("select-feature", e.features[0]);
-        });
-    }
+            map.U.addGeoJSON('points', points);
+            map.U.addCircle('points-circles', 'points', {
+                circleColor: 'hsl(330,100%,40%)',
+                circleRadius: {
+                    stops: [[10, 3], [12, 10]],
+                },
+            });
+            map.U.hoverPointer('points-circles');
+            map.on('click', 'points-circles', (e) => {
+                console.log(e);
+                EventBus.$emit('select-feature', e.features[0]);
+            });
+        },
+    },
 };
-import "mapbox-gl/dist/mapbox-gl.css";
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
